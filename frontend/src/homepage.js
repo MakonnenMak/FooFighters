e74c5e0d3947db770fcef400c4a08f3e5c1f16c4
Simmy Bhatia <sk8wt@virginia.edu>
2019-03-03 04:19:34 -0500

    temporarily commented out button, added CSS grid to the homepage to make receipts easier to deal with

--- frontend/src/homepage.js
+++ frontend/src/homepage.js
@@ -1,25 +1,87 @@
-import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+// import React, { Component } from 'react';
+// import Typography from '@material-ui/core/Typography';
+// import TextField from '@material-ui/core/TextField';
+// import Button from '@material-ui/core/Button';
 
-class Homepage extends Component {
-  render() {
-    return (
-      <div>
-        <Typography component="h2" variant="h2">YEET</Typography>
-        <TextField label="email" variant="outlined"/>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          style={{height: 56}}
-        >
-        Homepage
-        </Button>
+// class Homepage extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Button
+//           variant="contained"
+//           color="primary"
+//           size="large"
+//           style={{height: 100}, {fontSize: '40px'}}
+//         >
+//         Create New Receipt
+//         </Button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Homepage;
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: `${theme.spacing.unit * 5}px`,
+  },
+  paper: {
+    padding: theme.spacing.unit,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    whiteSpace: 'nowrap',
+    marginBottom: theme.spacing.unit,
+  },
+  divider: {
+    margin: `${theme.spacing.unit * 2}px 0`,
+  },
+});
+
+function CSSGrid(props) {
+  const { classes } = props;
+
+  return (
+
+    <div>
+        <Divider className={classes.divider} />
+
+      <div className={classes.container}>
+        <div style={{ gridColumnEnd: 'span 3' }}>
+          <Paper className={classes.paper}>receipt1</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 3' }}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 3' }}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 3' }}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 8' }}>
+          <Paper className={classes.paper}>xs=8</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 4' }}>
+          <Paper className={classes.paper}>xs=4</Paper>
+        </div>
       </div>
-    )
-  }
+    </div>
+  );
 }
 
-export default Homepage;
+CSSGrid.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CSSGrid);
