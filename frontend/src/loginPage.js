8c0de5a3cf67db13f0d446b7f0d9259cefa6ee84
AssadAijazi <assad.aijazi@gmail.com>
2019-03-03 03:25:02 -0500

    start frontend

---
+++ frontend/src/loginPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <Typography component="h2" variant="h2">Welcome</Typography>
+        <TextField label="email" variant="outlined"/>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+        >
+        Login
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default Login;
